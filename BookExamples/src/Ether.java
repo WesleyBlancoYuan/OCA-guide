c13b3700f90f236441d2180c20c8af5ae7423067
WesternGun <torresdyl@gmail.com>
2017-06-14 22:38:16 +0200

    Unit1 exercise

--- BookExamples/src/Ether.java
+++ BookExamples/src/Ether.java
@@ -1,5 +1,15 @@
-import com.Chair;
+import com.*;
 class Ether {
+	private int number;
+	private void printSA(String name) {
+		System.out.println(name);
+	}
     Chair c = new Chair();
+
+}
+
+class Another {
+	Ether e = new Ether();
+	e.number = 1;
 
 }
